a01cb50fd9d4d5d89fdfc0e2e6f7b3af352483ce
naganao <32755305+naganao@users.noreply.github.com>
2018-10-13 18:46:30 +0900

    rename and edit file
    rename and edit file

--- Multiple_linear_regression/ml.py
+++ Multiple_linear_regression/ml.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import linear_model
 from pymongo import MongoClient
 import json
+import math
 
 def main():
     con = MongoClient('localhost', 27017)
@@ -13,13 +14,15 @@ def main():
     data = pd.DataFrame(list(col.find()))
     # idを削除
     del data['_id']
-    print(data)
+    ### 検証用
+    # print(data)
     # data = pd.read_csv("data.csv", sep=",")
     pre_data = pd.read_csv("pre_data.csv", sep=",")
     clf = linear_model.LinearRegression()
     # 説明変数に "x1"のデータを使用
     X = data.loc[:, ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8']].values
-    print("X:\n", X)
+    ### 検証用
+    # print("X:\n", X)
     pre_X = pre_data.loc[:, ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8']].values
     # 目的変数に "x2"のデータを使用
     Y = data['x9'].values
@@ -29,11 +32,22 @@ def main():
     a = clf.coef_
     b = clf.intercept_
     # 回帰係数
-    print("回帰係数:", a)
-    print("切片:", b)
-    print("決定係数:", clf.score(X, Y))
-    print("予測値:", clf.predict(pre_X))
+    ### 検証用
+    # print("回帰係数:", a)
+    # print("切片:", b)
+    # print("決定係数:", clf.score(X, Y))
+    # print("予測値:", clf.predict(pre_X))
     # print("パラメータ:", clf.get_params())
+    predict = clf.predict(pre_X)[0]
+    # predict = 500
+    # predict = round(predict,0)
+    predict = math.floor(predict)
+    # 0~360にする
+    if 360 <= predict:
+        predict = 360
+    elif predict <= 0:
+        predict = 0
+    print(predict)
 
 if __name__ == "__main__":
     main()
